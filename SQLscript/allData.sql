fffa3dcefd7c191e5b1067fa989eb2365a781c4c
Zihao <wangzihao0377@gmail.com>
2024-11-21 14:46:50 +0100

    Revert "存在数据库就先 Drop 再导入 Dump 里的数据，即完全刷新数据库"
    This reverts commit a1324f2a6b8d7191ebe3dbedc84eab951d741746.

--- SQLscript/allData.sql
+++ SQLscript/allData.sql
@@ -15,10 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS `bips_wise24`;
-CREATE DATABASE bips_wise24;
-USE bips_wise24;
-
 --
 -- Table structure for table `antwort`
 --
